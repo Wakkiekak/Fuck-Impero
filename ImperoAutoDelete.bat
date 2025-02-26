00ae4107c817fed86300a86cd76b2b9c20ee9336
Wakkiekak <Wakkiekak@gmail.com>
2025-02-26 15:11:44 +0100

    Add files via upload

---
+++ ImperoAutoDelete.bat
@@ -0,0 +1,47 @@
+@echo off
+color 6
+cls
+echo --------- Impero Delete Tool ---------
+
+:: Ask the user if they want to open a link
+set /p userChoice=Wil je mij supporten voor mijn werk? (klik Y of N): 
+if /I "%userChoice%"=="Y" (
+    echo Dankje, heel aardig van je.
+    timeout /t 2 /nobreak
+    start https://www.youtube.com/@wakkiekak
+) else (
+    echo Ok dan niet...
+)
+
+echo.
+
+:: Check if Impero is installed
+echo Kijken of Impero is geinstalleerd...
+wmic product get name | findstr /I "Impero Client" >nul
+if %errorlevel%==0 (
+    echo Impero Client is geinstalleerd.
+    echo.
+
+    :: Terminate the processes and uninstall
+    echo Closing Impero processes...
+    taskkill /F /IM ImperoClient.exe
+    taskkill /F /IM BackdropClient.exe
+
+    timeout /t 2 /nobreak
+
+    echo Uninstalling Impero Client and Backdrop Client...
+    wmic product where "name='Impero Client'" call uninstall /nointeractive
+    wmic product where "name='Backdrop Client'" call uninstall /nointeractive
+
+    echo Removing leftover directories...
+    rd /s /q "C:\Program Files\Impero Software\Backdrop Client"
+    rd /s /q "C:\Program Files\BackdropClient"
+    rd /s /q "C:\Program Files (x86)\Impero"
+    rd /s /q "C:\Program Files (x86)\BackdropClient"
+
+    echo Programs removed.
+) else (
+    echo Impero Client is niet geinstalleerd. 
+)
+
+pause
